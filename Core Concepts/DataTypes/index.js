fbca5eacc7dc04928ffae85d0d2b947bc30c778b
Timotei Dumitru <dumitru.timy@gmail.com>
2020-07-14 18:29:42 +0100

    Data types

---
+++ Core Concepts/DataTypes/index.js
@@ -0,0 +1,28 @@
+// - String -> sequence of characters, used for text
+var firstName = "John";
+console.log(firstName);
+
+var secondName = "Smith";
+console.log(secondName);
+
+// - Number -> float point numbers, for decimals and integers
+var age = 30;
+console.log(age);
+
+// - Boolean -> logical data type that can only be true or false
+var married = true;
+console.log(married);
+
+// - Undefined -> data type of a variable that does not have a value yet
+var job;
+console.log(job);
+
+// Data Types
+
+// - String -> sequence of characters, used for text
+// - Number -> float point numbers, for decimals and integers
+// - Boolean -> logical data type that can only be true or false
+// - Undefined -> data type of a variable that does not have a value yet
+// - Null -> also means 'non-existent'
+
+// note! -> JavaScript has dynamic typing which means data types are automatically asigned to variables
