db1acd60e8f00ee7257aef68604bc2879d8c213c
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-04 20:57:22 +0100

    made a function to keep the score of the user input

--- Quiz Game/script.js
+++ Quiz Game/script.js
@@ -72,12 +72,21 @@
     }
   };
 
-  Question.prototype.checkAnswer = function (ans) {
-    if (ans == this.correct) {
+  Question.prototype.checkAnswer = function (ans, callback) {
+    var sc;
+    if (ans === this.correct) {
       console.log("Correct answer");
+      sc = callback(true);
     } else {
       console.log("Wrrong answer, give it another try? :)");
+      sc = callback(false);
     }
+    this.displayScore(sc);
+  };
+
+  Question.prototype.displayScore = function (score) {
+    console.log("Your current score is: " + score);
+    console.log("#######################################");
   };
 
   // questions
@@ -99,6 +108,20 @@
 
   var questions = [q1, q2, q3];
 
+  function score() {
+    var sc = 0;
+    return function (correct) {
+      if (correct) {
+        sc++;
+      } else {
+        sc--;
+      }
+      return sc;
+    };
+  }
+
+  var keepScore = score();
+
   function nextQuestion() {
     var n = Math.floor(Math.random() * questions.length);
 
@@ -109,7 +132,7 @@
     );
 
     if (answer !== "exit") {
-      questions[n].checkAnswer(parseInt(answer));
+      questions[n].checkAnswer(parseInt(answer), keepScore);
 
       nextQuestion();
     }
