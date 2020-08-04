ed09174a3e2faaef4f70f7fd686ea264f8a0f356
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-04 20:04:41 +0100

    prototyping the console output to Questions constructor

--- Quiz Game/script.js
+++ Quiz Game/script.js
@@ -1,1 +1,37 @@
-console.log("it is WORKING!!");
+// questions constructor
+function Question(question, answers, correct) {
+  this.question = question;
+  this.answers = answers;
+  this.correct = correct;
+}
+
+Question.prototype.displayQuestion = function () {
+  console.log(this.question);
+
+  for (var i = 0; i < this.answers.length; i++) {
+    console.log(i + ":" + this.answers[i]);
+  }
+};
+
+// questions
+var q1 = new Question(
+  "Is JavaScript the coolest programming language in the world?",
+  ["yes", "no"],
+  0
+);
+var q2 = new Question(
+  "What's the name of this course teacher?",
+  ["John", "Mike", "Johna", "Elena"],
+  2
+);
+var q3 = new Question(
+  "What does best describe coding?",
+  ["Borring", "Hard", "Fun", "Tedious"],
+  2
+);
+
+var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
