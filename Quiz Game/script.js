356d4c34cce15279ce62f41d1b677898bbd6b539
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-04 20:19:41 +0100

    made the code to not interact with others programmers code(made use of IIFE)

--- Quiz Game/script.js
+++ Quiz Game/script.js
@@ -1,49 +1,51 @@
-// questions constructor
-function Question(question, answers, correct) {
-  this.question = question;
-  this.answers = answers;
-  this.correct = correct;
-}
-
-Question.prototype.displayQuestion = function () {
-  console.log(this.question);
-
-  for (var i = 0; i < this.answers.length; i++) {
-    console.log(i + ":" + this.answers[i]);
+(function () {
+  // questions constructor
+  function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
   }
-};
 
-Question.prototype.checkAnswer = function (ans) {
-  if (ans == this.correct) {
-    console.log("Correct answer");
-  } else {
-    console.log("Wrrong answer, give it another try? :)");
-  }
-};
+  Question.prototype.displayQuestion = function () {
+    console.log(this.question);
 
-// questions
-var q1 = new Question(
-  "Is JavaScript the coolest programming language in the world?",
-  ["yes", "no"],
-  0
-);
-var q2 = new Question(
-  "What's the name of this course teacher?",
-  ["John", "Mike", "Johna", "Elena"],
-  2
-);
-var q3 = new Question(
-  "What does best describe coding?",
-  ["Borring", "Hard", "Fun", "Tedious"],
-  2
-);
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i + ":" + this.answers[i]);
+    }
+  };
 
-var questions = [q1, q2, q3];
+  Question.prototype.checkAnswer = function (ans) {
+    if (ans == this.correct) {
+      console.log("Correct answer");
+    } else {
+      console.log("Wrrong answer, give it another try? :)");
+    }
+  };
 
-var n = Math.floor(Math.random() * questions.length);
+  // questions
+  var q1 = new Question(
+    "Is JavaScript the coolest programming language in the world?",
+    ["yes", "no"],
+    0
+  );
+  var q2 = new Question(
+    "What's the name of this course teacher?",
+    ["John", "Mike", "Johna", "Elena"],
+    2
+  );
+  var q3 = new Question(
+    "What does best describe coding?",
+    ["Borring", "Hard", "Fun", "Tedious"],
+    2
+  );
 
-questions[n].displayQuestion();
+  var questions = [q1, q2, q3];
 
-var answer = parseInt(prompt("Please select the correct answer."));
+  var n = Math.floor(Math.random() * questions.length);
 
-questions[n].checkAnswer(answer);
+  questions[n].displayQuestion();
+
+  var answer = parseInt(prompt("Please select the correct answer."));
+
+  questions[n].checkAnswer(answer);
+})();
