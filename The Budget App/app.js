606c3fef31c291a9c4cb09e38e42581389052af5
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-06 20:05:46 +0100

    implementing module pattern

--- The Budget App/app.js
+++ The Budget App/app.js
@@ -6,7 +6,19 @@ var budgetController = (function () {
   };
   return {
     publicTest: function (b) {
-      console.log(add(b));
+      return add(b);
     },
   };
 })();
+
+var UIController = (function () {})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function () {
+      console.log(z);
+    },
+  };
+})(budgetController, UIController);
