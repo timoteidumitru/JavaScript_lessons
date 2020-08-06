e228426fae9eda36cb24b172f18e2354626ed9fa
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-06 19:53:19 +0100

    more exemples of closures

--- Advenced JavaScript/Closure/index.js
+++ Advenced JavaScript/Closure/index.js
@@ -14,3 +14,18 @@ var retirementGermany = retirement(65);
 retirementUS(1990);
 retirementUK(1990);
 retirementGermany(1990);
+
+// more exemple of using closures
+
+var budgetController = (function () {
+  var x = 23;
+
+  var add = function (a) {
+    return x + a;
+  };
+  return {
+    publicTest: function (b) {
+      console.log(add(b));
+    },
+  };
+})();
