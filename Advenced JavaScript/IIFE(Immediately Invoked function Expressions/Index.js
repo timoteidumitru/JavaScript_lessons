ae61531cd98e6fb002109ce7d9dbcdf162b329c8
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-07 22:16:23 +0100

    some IIFE use in real world app scenario

--- Advenced JavaScript/IIFE(Immediately Invoked function Expressions/Index.js
+++ Advenced JavaScript/IIFE(Immediately Invoked function Expressions/Index.js
@@ -15,3 +15,30 @@ function game() {
   var score = Math.round(Math.random() * 10);
   console.log(score >= 5 - goodLuck);
 })(5);
+
+// more of IIFE
+
+var budgetController = (function () {
+  var x = 23;
+
+  var add = function (a) {
+    return x + a;
+  };
+  return {
+    publicTest: function (b) {
+      return add(b);
+    },
+  };
+})();
+
+var UIController = (function () {})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+  var z = budgetCtrl.publicTest(5);
+
+  return {
+    anotherPublic: function () {
+      console.log(z);
+    },
+  };
+})(budgetController, UIController);
