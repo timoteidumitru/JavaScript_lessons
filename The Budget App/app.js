194c3176a7ecfe9071d824d23b361d976407de1d
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-08 20:29:11 +0100

    basic setup for add items

--- The Budget App/app.js
+++ The Budget App/app.js
@@ -1,24 +1,29 @@
+// BUDGET CONTROLLER
 var budgetController = (function () {
-  var x = 23;
-
-  var add = function (a) {
-    return x + a;
-  };
-  return {
-    publicTest: function (b) {
-      return add(b);
-    },
-  };
+  // some code
 })();
 
-var UIController = (function () {})();
+// UI CONTROLLER
+var UIController = (function () {
+  // some code
+})();
 
+// GLOBAL ALL CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
-
-  return {
-    anotherPublic: function () {
-      console.log(z);
-    },
+  var ctrlAddItem = function () {
+    // 1. get the field input data
+    // 2. add the item to budget controller
+    // 3. add the item to the UI
+    // 4. calculate the budget
+    // 5. display the budget on the UI
+    console.log("its works");
   };
+
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+  document.addEventListener("keypress", function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
