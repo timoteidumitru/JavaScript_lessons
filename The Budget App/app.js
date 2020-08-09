0108f7451bc71fe3620eb0fe660890ff540cbbd2
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-09 10:24:06 +0100

    add basic functionality to user inputs

--- The Budget App/app.js
+++ The Budget App/app.js
@@ -5,21 +5,42 @@ var budgetController = (function () {
 
 // UI CONTROLLER
 var UIController = (function () {
-  // some code
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, //
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value,
+      };
+    },
+    getDOMStrings: function () {
+      return DOMStrings;
+    },
+  };
 })();
 
 // GLOBAL ALL CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMStrings();
   var ctrlAddItem = function () {
     // 1. get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. add the item to budget controller
     // 3. add the item to the UI
     // 4. calculate the budget
     // 5. display the budget on the UI
-    console.log("its works");
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener("click", ctrlAddItem);
 
   document.addEventListener("keypress", function (e) {
     if (e.keyCode === 13 || e.which === 13) {
