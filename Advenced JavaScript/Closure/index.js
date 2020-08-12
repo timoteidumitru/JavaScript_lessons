4488c0bc614d6fb706789c8c92aaf8610b847cdf
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-12 20:45:23 +0100

    more examples of closures and IIFE\'s

--- Advenced JavaScript/Closure/index.js
+++ Advenced JavaScript/Closure/index.js
@@ -29,3 +29,35 @@ var budgetController = (function () {
     },
   };
 })();
+
+// more of closures + IIFE
+var controller = (function (budgetCtrl, UICtrl) {
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDOMStrings();
+    document
+      .querySelector(DOM.inputButton)
+      .addEventListener("click", ctrlAddItem);
+
+    document.addEventListener("keypress", function (e) {
+      if (e.keyCode === 13 || e.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var ctrlAddItem = function () {
+    // 1. get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+    // 2. add the item to budget controller
+    // 3. add the item to the UI
+    // 4. calculate the budget
+    // 5. display the budget on the UI
+  };
+
+  return {
+    init: function () {
+      setupEventListeners();
+    },
+  };
+})(budgetController, UIController);
