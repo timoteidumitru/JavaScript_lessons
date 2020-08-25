9dda0bc2764f0ab942b451e3aa5b6ef908645e88
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-25 18:27:54 +0100

    Blocks and IIFEs in ES6

---
+++ ES6/Blocks and IIFEs/index.js
@@ -0,0 +1,19 @@
+// Lecture: Blocks and IIFEs
+
+// ES6 IIFE
+
+{
+  const a = 1;
+  let b = 2;
+  var c = 3;
+}
+// console.log(a, b);
+console.log(c);
+
+// ES5 IIFE
+
+// (function () {
+//     var c = 5;
+//   }
+// )();
+// console.log(c);
