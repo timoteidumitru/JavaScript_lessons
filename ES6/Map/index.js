dfed3a34262b2fb07fe7687b68c7a97801b463d3
Timotei Dumitru <dumitru.timy@gmail.com>
2020-08-31 18:00:48 +0100

    Maps data structure

---
+++ ES6/Map/index.js
@@ -0,0 +1,41 @@
+// Lecture: Maps
+
+const question = new Map();
+
+question.set(
+  "question",
+  "What is the official name of the latest major JavaScript version"
+);
+question.set(1, "ES5");
+question.set(2, "ES6");
+question.set(3, "ES2015");
+question.set(4, "ES7");
+question.set("correct", 3);
+question.set(true, "Correct answer :D");
+question.set(false, "Wrong, please try again!");
+
+// console.log(question.get("question"));
+// console.log(question.size);
+
+if (question.has(4)) {
+  // question.delete(4);
+  // console.log("Answer 4 is here");
+}
+
+// question.clear();
+
+// console.log(question);
+
+question.forEach((value, key) => {
+  // console.log(`This is ${key}, and is set to ${value}`);
+});
+
+for (let [key, value] of question.entries()) {
+  if (typeof key === "string") {
+    console.log(`answer ${key}: ${value}`);
+  }
+}
+
+const ans = parseInt(prompt("Write the correct answer"));
+
+console.log(question.get(ans === question.get("correct")));
