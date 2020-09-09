a3d6194c336e61ca2c1b0550e43d861fcad8d27b
Timotei Dumitru <dumitru.timy@gmail.com>
2020-09-09 19:53:39 +0100

    fetch with async await and try/catch method to get data from an public API

--- Asyncronous JavaScript/Ajax - API/index.js
+++ Asyncronous JavaScript/Ajax - API/index.js
@@ -5,9 +5,23 @@ fetch("https://cat-fact.herokuapp.com/facts")
   .then((data) => {
     let items = { ...data };
     theCatFact = items.all[1];
-    console.log(theCatFact);
-    console.log(
-      `${theCatFact.user.name.first} ${theCatFact.user.name.last} just learn the following cat fact: ${theCatFact.text}`
-    );
+    // console.log(theCatFact);
+    // console.log(
+    //   `${theCatFact.user.name.first} ${theCatFact.user.name.last} just learn the following cat fact: ${theCatFact.text}`
+    // );
   })
   .catch((err) => console.log(err));
+
+async function getFunFactsCats() {
+  try {
+    const result = await fetch("https://cat-fact.herokuapp.com/facts");
+    const data = await result.json();
+    console.log(data.all[1].text);
+    console.log(data.all[2].text);
+    console.log(data.all[3].text);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getFunFactsCats();
