4a3a152fd21bdd0491a7e288e4bd26e829b89afd
Timotei Dumitru <dumitru.timy@gmail.com>
2020-09-15 20:01:16 +0100

    setup babel

--- Recipe Finder App/src/js/index.js
+++ Recipe Finder App/src/js/index.js
@@ -1,6 +1,9 @@
 // Global app controller
 import x from "./test";
 
+const y = 23;
+
 console.log(
-  `I imported ${x} from another module! >> test file aka fake module <<`
+  `I imported ${x} from another module! >> test file aka fake module <<
+  Variable x is equal to ${y}`
 );
--- Recipe Finder App/webpack.config.js
+++ Recipe Finder App/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: ["babel-polyfill", "./src/js/index.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "js/bundle.js",
@@ -16,4 +16,15 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
