e1fa0c906cb78adf6c9f25a74e95acc0aae5543a
Timotei Dumitru <dumitru.timy@gmail.com>
2020-09-17 19:38:01 +0100

    made the call to the API using axios library

--- Recipe Finder App/src/js/index.js
+++ Recipe Finder App/src/js/index.js
@@ -1,1 +1,15 @@
 // Global app controller
+import axios from "axios";
+
+async function getresults(query) {
+  try {
+    const res = await axios(
+      `https://forkify-api.herokuapp.com/api/search?q=${query}`
+    );
+    console.log(res.data.recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getresults("pizza");
