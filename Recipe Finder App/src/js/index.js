87404855f6b4b29a85ccc5c095f904598e2ddbec
Timotei Dumitru <dumitru.timy@gmail.com>
2020-09-23 19:58:05 +0100

    add latest updates on the Recipe Finde App

--- Recipe Finder App/src/js/index.js
+++ Recipe Finder App/src/js/index.js
@@ -1,15 +1,36 @@
 // Global app controller
-import axios from "axios";
+import Search from "./models/Search";
 
-async function getresults(query) {
-  try {
-    const res = await axios(
-      `https://forkify-api.herokuapp.com/api/search?q=${query}`
-    );
-    console.log(res.data.recipes);
-  } catch (error) {
-    alert(error);
+/* Global state of the app
+    -Search object
+    -Current recipe object
+    -Shopping list Object
+    -Liked recipes
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get the query from view
+  const query = "pizza"; //TODO
+
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+    // 3) Prepare UI for results
+
+    // 4) Search for recipes
+    await state.search.getResults();
+    // 5) Render the results on UI
+    console.log(state.search.result);
   }
-}
+};
 
-getresults("pizza");
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
+
+// const search = new Search("pizza");
+
+// search.getResults();
