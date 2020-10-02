5e0447681972f793b780ef041cfc29db374d389b
Timotei Dumitru <dumitru.timy@gmail.com>
2020-10-02 19:27:06 +0100

    add recipe model

---
+++ Recipe Finder App/src/js/models/Recipe.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export default class Recipe {
+  constructor(id) {
+    this.id = id;
+  }
+  async getRecipe() {
+    try {
+      const res = await axios(
+        `https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
+      );
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.publisher;
+      this.img = res.data.recipe.image_url;
+      this.url = res.data.recipe.source_url;
+      this.ingredients = res.data.recipe.ingredients;
+    } catch (err) {
+      console.log(err);
+      alert("something went wrong :(");
+    }
+  }
+
+  calcTime() {
+    // Assuming that we need 15 min for each 3 ingredients
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings() {
+    this.servings = 4;
+  }
+}
